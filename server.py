ef43415dce8782d1646e6c9d78799650a630aa70
wipfli <wipfli@phys.ethz.ch>
2021-03-04 22:08:17 +0100

    Update server

--- server.py
+++ server.py
@@ -28,9 +28,6 @@ def get_bbox(longitude0, latitude0, longitude1, latitude1):
 def get_name(filename):
     return os.path.basename(filename).split('.')[0]
 
-def get_layer_names(layers):
-    return [layer for layer in layers]
-
 def do_query(layers, layer, longitude0, latitude0, longitude1, latitude1):
     bbox = get_bbox(longitude0, latitude0, longitude1, latitude1)
     if layer not in layers:
@@ -38,14 +35,41 @@ def do_query(layers, layer, longitude0, latitude0, longitude1, latitude1):
     selection = select(layers[layer], bbox)
     return selection.to_json()
 
-def get_filenames(folder):
-    return glob.glob(folder + '*.shp')
+def get_layer_names(folder):
+    return [g.split('/')[-1] for g in glob.glob(folder + '/*')]
+
+def find_file(layer_name, folder):
+    files = glob.glob(folder + layer_name + '/*.gpkg')
+    if len(files) > 0:
+        return files[0]
+    files = glob.glob(folder + layer_name + '/*LV95*/*.shp')
+    if len(files) > 0:
+        return files[0]
+    files = glob.glob(folder + layer_name + '/*.shp')
+    if len(files) > 0:
+        return files[0]
+    files = glob.glob(folder + layer_name + '/*.json')
+    if len(files) > 0:
+        return files[0]
+    return None
 
 layers = {}
 
 folder = './shapefiles/'
-for filename in get_filenames(folder):
-    layers[get_name(filename)] = read_shp(filename)
+layer_names = get_layer_names(folder)
+for layer_name in layer_names:
+    filename = find_file(layer_name, folder)
+    if filename:
+        print('reading layer', layer_name)
+        try:
+            layers[layer_name] = read_shp(filename)
+        except ValueError:
+            print(f'error, skipping {layer_name}')
+    else:
+        print('format unknown, skipping', layer_name)
+
+print('skipped in total', (len(layer_names) - len(layers)), 'layers')
+print('loaded in total', len(layers), 'layers')
 
 app = FastAPI()
 
@@ -59,7 +83,7 @@ app.add_middleware(
 
 @app.get('/layers')
 async def layer_names():
-    return get_layer_names(layers)
+    return [layer for layer in layers]
 
 @app.get('/query')
 async def query(
